50b3cd87aee9bfce2c81fd5210e10e6bb8d83e76
jiujiuwo <1203574299@qq.com>
2018-05-10 11:24:00 +0800

    在eval时输出inference的结果2

--- ImageEval.py
+++ ImageEval.py
@@ -94,11 +94,10 @@ def eval_once(saver, summary_writer, top_k_op, summary_op,logits,labels):
       while step < num_iter and not coord.should_stop():
         predictions = sess.run([top_k_op])
         print('labels:')
-        print(labels)
-        #logits = sess.run(logits)
+        print(sess.run(labels))
+        logits = sess.run(logits)
         print('logits')
-        print(logits)
-        #print(np.argmax(logits,axis=1))
+        print(np.argmax(logits,axis=1))
         true_count += np.sum(predictions)
         step += 1
         print(step)
